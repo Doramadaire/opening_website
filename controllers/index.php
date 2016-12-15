d92a295de28051b7d12a56001b50393fc321e099
Claverie Guilhem <gclaverie@mourepiane143.etu.ec-m.fr>
2016-12-15 18:12:17 +0100

    retour à une version sans session

--- controllers/index.php
+++ controllers/index.php
@@ -1,33 +1,4 @@
 <?php	
-
-	//Gestion des sessions utilisateurs
-	$login = isset($_POST['login']) ? $_POST['login'] : ''; 
-	$password = isset($_POST['password']) ? $_POST['password'] : '';
-
-	/*
-	Si le champ login n'est pas rempli => erreur 1   
-	Si le champ mdp n'est pas rempli => erreur 3
-	Si le mdp ne correspond pas au login => erreur 2
-
-	Si tout est ok, on crée la session.
-	*/
-	if($login == '') {
-		header('Location: index.php?error=1');
-		} else if($password != "toto" && $password != '') {
-			header('Location: index.php?error=2&password='.$password);
-		} else if($password =='') {
-			header('Location: index.php?error=3');
-		} else {
-			session_start();
-			$_SESSION['login']=$login;
-			$_SESSION['password']=$password;
-			$_SESSION['logged']=true;
-			/* $_SESSION['statut']='membre' ou 'admin'  info obtenue à partir d'une requete SQL*/		
-			header('Location: index.php');
-		}
-
-
-
 	$unMail = "lolilol@gmail.com";
 	$unUser = new User(0, $unMail, 5, "2001-01-01");
 	$unUser2 = new User(0, "lol", 4, "2001-01-01");
