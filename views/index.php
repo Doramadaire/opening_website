f9a71a514717cd2f5b697bd531df2427d17fa2b0
314RO <thehauntedcomputer@gmail.com>
2017-01-05 14:40:05 +0100

    Essai
    modification mineure. Mais je teste git

--- views/index.php
+++ views/index.php
@@ -3,18 +3,51 @@
 		<title>
 			Opening 
 		</title>
+		<link rel="stylesheet" href="css/index.css" type="text/css">
+		
 	</head>
 	<body>
-		<br>
+		 <div class="top">
 		Voici le site opening book
-		<br> 
-		<?php if ($user_logged != false) { ?>
-			Bien joué beau gosse! Alors comme ça je peux te contacter en t'envoyant un mail à <?php echo $user_logged->getUserMail();; ?> ?
+		 </div>
+		<br/> 
+		<div class="action">
+		<?php if ($user_logged) { ?>
+			Bonjour <?php echo $user_logged->getUserMail();; ?>
+			Vous êtes connecté en tant  
+		<?php 	switch($_SESSION['user_logged']->getUserStatus()) 
+					{
+							     case 2: echo "que visiteur"; ?>
+					Vous pouvez : <br/> 
+						<ul> <li><a href="images.html">Parcourir les oeuvres (extraits)</a></li> 
+							 <li><a href="images.html">Gérer votre compte</a></li> </ul>
+							 
+					<?php break; case 3: echo "que cotisant";  ?>
+					Vous pouvez : <br/> 
+						<ul> <li><a href="images.html">Parcourir les oeuvres</a></li> 
+							 <li><a href="images.html">Gérer votre compte</a></li> </ul>
+							 
+					<?php break; case 4:	echo "qu'auteur";	?>
+					Vous pouvez : <br/> 
+					
+						<ul> <li><a href="images.html">Parcourir les oeuvres</a></li> 
+							 <li><a href="images.html">Gérer votre compte</a></li> 
+							 <li><a href="images.html">Gérer vos oeuvres</a></li>  </ul>
+							 
+					<?php break; case 5:	echo "qu'administrateur"; ?>
+					Vous pouvez : <br/> 
+						<ul> <li><a href="images.html">Parcourir les oeuvres</a></li> 
+							 <li><a href="images.html">Gérer les membres></li> 
+							 <li><a href="images.html">Gérer les oeuvres</a></li>  
+							 <li><a href="images.html">?</a></li> </ul>
+							 
+					<?php break;} ?>
+			
 			<form action="" method="POST">
 				<input type="submit" name="loggout_form" value="Se déconnecter">
 			</form>
 		<?php } else { ?>
-			Bouh, t'es pas connecté! Allez spa grave on va arranger ça
+			Vous n'êtes pas connecté. 
 			<form action="" method="POST">
 				<input type="text" name="mail" placeholder="e-mail"> <br>
 				<input type="password" name="password" placeholder="mot de passe">  <br>
@@ -24,6 +57,7 @@
 			if (isset($error)) {
 				echo $error;
 			} ?>
+			</div> <!-- action-->
 	</body>
 
 </html>
