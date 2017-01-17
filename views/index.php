f12b3c327892a4f2fe84966058fc236afb01a4d2
314RO <thehauntedcomputer@gmail.com>
2017-01-18 00:00:35 +0100

    Header et bootstrap
    Il y a maintenant tous les fichiers pour bootstrap (pour le menu
    déroulant par exemple)
    Et la navbar. une infobulle, quelques liens, mais pas tout.

--- views/index.php
+++ views/index.php
@@ -1,12 +1,24 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 	<head>
-	<meta charset="UTF-8">
-		<title>
-			Opening 
-		</title>
-		<link rel="stylesheet" href="css/index.css" type="text/css">
-		
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<title>OPENING</title>
+
+		<!-- Bootstrap -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/index.css" rel="stylesheet">
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
 	</head>
+  
 	<body>
 		<?php include("header.php"); ?> 
 
@@ -60,6 +72,16 @@
 			<a href="book_viewer.php">Parcourir les oeuvres (seulement des extraits)</a>
 		<?php } ?>
 		</div> <!-- action-->
-	</body>
+		
+		
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
 
+	</body>
+	
+	
+	
+    
 </html>
