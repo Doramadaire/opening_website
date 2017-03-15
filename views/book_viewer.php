4e118425d7e9b7bba80caf6861d5b7def42109a6
Claverie Guilhem <gclaverie@calade163.etu.ec-m.fr>
2017-03-15 19:50:45 +0100

    ajout wowbook

--- views/book_viewer.php
+++ views/book_viewer.php
@@ -9,10 +9,51 @@
 		
 		 <!-- Scripts liseuse -->
         <script src="js/jquery.min.js"></script>
-        <script src="js/main.js"></script>
+        <script src="js/main.js"></script>  
+        <!-- Wowbook -->
+        <script type="text/javascript" src="./wow_book/pdf.combined.min.js"></script>
+        <script type="text/javascript" src="./wow_book/wow_book.min.js"></script>
+        <link rel="stylesheet" href="./wow_book/wow_book.css" type="text/css" />
 		
         <!-- Fichiers maison -->
-        <link rel="stylesheet" href="css/book_viewer.css" type="text/css">		
+        <link rel="stylesheet" href="css/book_viewer.css" type="text/css">	
+
+        <script>
+        $(function(){
+            var bookOptions1 = {
+                height   : 500,
+                width    : 800,
+                pageNumbers: false,
+                
+                centeredWhenClosed : true,
+                toolbar : "lastLeft, left, right, lastRight, zoomin, zoomout, slideshow, fullscreen, thumbnails",
+                lightbox: "#show_wowbook",
+                thumbnailsPosition : 'left',                
+                /*
+                           
+                hardcovers : true, 
+                responsiveHandleWidth : 50,
+                
+                containerPadding: "20px",
+                */
+                // The pdf and your webpage must be on the same domain
+                pdf: "resources/books/OPENINGBOOK_001.pdf"
+            };
+            // creates the book
+            $('#wowbook1').wowBook(bookOptions1);
+            var bookOptions2 = {
+                height   : 1000,
+                width    : 1200,
+                pageNumbers: false,                
+                centeredWhenClosed : true,
+                container: true,
+                gutterShadow: false,
+                toolbar : "lastLeft, left, right, lastRight, zoomin, zoomout, slideshow, fullscreen, thumbnails",              
+                pdf: "resources/books/OPENINGBOOK_002.pdf"
+            };
+            $('#wowbook2').wowBook(bookOptions2);
+        })
+    </script>	
 	</head>
 	<body oncontextmenu="return false">
 	<?php include("include/header.php"); ?>
@@ -54,6 +95,7 @@
                 <div class="row fliphtml5">
                     <iframe title="opening-book" frameborder="0" type="text/html" width="700" height="700" src="http://online.fliphtml5.com/izscb/rgzc/" allowfullscreen="true" scroll="no" marginwidth="0" marginheight="0"></iframe>
                 </div>                
+
     		    <?php } else {
     				if ($user_logged->getUserStatus() >= 3) { ?>
     			<!-- DEVDEVDEV t'as le droit de foire le book complet si t'es loggé avec abonnement à jour, ou si t'es auteur et que c'est ton book, ou que t'es admin? -->
@@ -68,8 +110,10 @@
         	    </div>
                 <br>
                 <div class="row fliphtml5">
-                    <iframe title="opening-book" frameborder="0" type="text/html" width="700" height="700" src="http://online.fliphtml5.com/zkoe/irhd/" allowfullscreen="true" scroll="no" marginwidth="0" marginheight="0"></iframe>
-                </div>				
+                    <!-- BOOK1 : http://online.fliphtml5.com/obqs/yyip/ -->
+                    <!-- BOOK2 : http://online.fliphtml5.com/zkoe/irhd/ -->
+                    <iframe title="opening-book" frameborder="0" type="text/html" width="700" height="700" src="http://online.fliphtml5.com/obqs/yyip/" allowfullscreen="true" scroll="no" marginwidth="0" marginheight="0"></iframe>
+                </div>                	
     		<?php  		} else { ?>
     
     	        <div class="docs-galley">    
@@ -91,6 +135,16 @@
     	    </div>	
     	</div>
         <!-- DEVEVDV mettre une marge en CSS plutot que un br? -->
+        <!--
+        <div class='wowbook_container'>
+            <div id="wowbook"></div>
+        </div>
+         -->
+        <img id="show_wowbook" src="resources/jpg/page_0000.jpg" height="600px" width="auto">
+        <div id="wowbook1"></div> <!-- celui avec lightbox et les liens internes -->
+        <div class='wowbook_container'>
+            <div id="wowbook2"></div>
+        </div>
         <div class="row">
             <div class="thumbnail">
                 <h3>À propos</h3>
@@ -110,18 +164,17 @@
             </div>
         </div>
     </div>
-
 	
-<!-- The Modal : boite de dialoge d'info de la liseuse -->
-<div id="myModal" class="modal">
-  <!-- Modal content -->
-  <div class="modal-content">
-    <span class="close">×</span>
-    <p>La fenetre d'aide avec du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte</p>
-  </div>
-</div>
+    <!-- The Modal : boite de dialoge d'info de la liseuse -->
+    <div id="myModal" class="modal">
+      <!-- Modal content -->
+      <div class="modal-content">
+        <span class="close">×</span>
+        <p>La fenetre d'aide avec du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte,du texte</p>
+      </div>
+    </div>
 	
-<br><br><br><br>
+    <br><br><br><br>
 	<?php include("include/footer.php"); ?>
 
     <script src="js/viewer.js"></script>
