b55e7cec4abfd90f25f303d8d3c0d3c907d0359d
Claverie Guilhem <gclaverie@calade161.etu.ec-m.fr>
2017-03-15 11:29:56 +0100

    modif mineure navbar

--- views/include/header.php
+++ views/include/header.php
@@ -1,19 +1,19 @@
-<div class="page-header">
-  <div class="navbar-wrapper">     
-    <!-- Le "nom" de la navbar, et l'allure du bouton sur mobile-->
-    <nav class="navbar navbar-default">
-      <!-- Les contenu de la navbar -->
-      <div class="container-fluid">
+<div class="container-fluid">
+  <div class="page-header">
+    <div class="navbar-wrapper">     
+      <!-- Le "nom" de la navbar, et l'allure du bouton sur mobile-->
+      <nav class="navbar navbar-default">
+        <!-- Les contenu de la navbar -->
         <!-- Brand and toggle get grouped for better mobile display -->    
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav-collapse" aria-expanded="false">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>  
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>  
 		  
-		  <a class="navbar-left" href="index.php"><img class="img-fluid" id="logo" alt="logo" src="resources/logo_cropped.png"></a>        
+	 	      <a class="navbar-left" href="index.php"><img class="img-fluid" id="logo" alt="logo" src="resources/logo_cropped.png"></a>        
               
 		    </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
@@ -26,20 +26,20 @@
             <li><a href="join.php"><?php echo TXT_ADHERER; ?></a></li>
           </ul>
 
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="https://www.facebook.com/opening-book-872866662728445/" target="_blank">Facebook</a></li>
-            <li><a href="https://twitter.com/opening_asso" target="_blank" >Twitter</a></li>
-            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?php echo TXT_LANGUE; ?><span class="caret"></span></a>
-              <ul class="dropdown-menu">
-                <li><a href="?lang=fr">Fr</a></li>
-                <li><a href="?lang=en">En</a></li>
-              </ul>
-            </li>
-          </ul>
+         <ul class="nav navbar-nav navbar-right">
+           <li><a href="https://www.facebook.com/opening-book-872866662728445/" target="_blank">Facebook</a></li>
+           <li><a href="https://twitter.com/opening_asso" target="_blank" >Twitter</a></li>
+           <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?php echo TXT_LANGUE; ?><span class="caret"></span></a>
+             <ul class="dropdown-menu">
+               <li><a href="?lang=fr">Fr</a></li>
+               <li><a href="?lang=en">En</a></li>
+             </ul>
+           </li>
+         </ul>
           
-        </div><!-- /.navbar-collapse -->
+       </div><!-- /.navbar-collapse -->
 
-      </div><!-- /.container-fluid -->
-    </nav>
+      </nav>
+    </div>
   </div>
 </div>
