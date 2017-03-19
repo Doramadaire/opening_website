343e35ef9998904eb29aeaaa446b22b52e8c699c
Doramadaire <guilhem-claverie@orange.fr>
2017-03-19 20:56:40 +0100

    nouvelle page : news!

--- views/include/en-lang.php
+++ views/include/en-lang.php
@@ -148,8 +148,14 @@
                     <p>Opening book est un format pré-défini de douze pages offertes à un artiste pour y exposer douze œuvres et les diffuser sur tout type d’écran : ordinateur, tablette, smartphone. La collection propose une approche directe des œuvres grâce à la qualité des reproductions, à une mise en page épurée construite autour de l’œuvre et du détail et à un mode de navigation conçu pour le numérique.</p>
                     <p>Opening book est aussi un dispositif qui offre aux artistes des outils essentiels pour promouvoir leur travail : des reproductions photographiques de qualité professionnelle, une interface de gestion en ligne des archives photographiques (espace membres) et l’inscription dans un large réseau de diffusion.</p>
                     <a href=".'"join.php"'.">Pour consulter les opening books dans leur intégralité, devenez adhérents.</a>");
+
+	//---------------------------------------------------------
+ 	// news.php
+ 	//---------------------------------------------------------	
  
- 
+	define('TXT_SECTION_ACTUALITES', 'Actualités');
+	define('TXT_ACTUALITES_VIDE', "Il n'y a aucune nouvelle à afficher");
+
 	//---------------------------------------------------------
  	// contact.php
  	//---------------------------------------------------------	
---
+++ views/news.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include("include/html_header.php"); ?>
+        <title>Opening</title>
+        <!-- Import des fichiers spécifiques à cette page -->
+        <!-- <link href="css/news.css" rel="stylesheet"> -->
+    </head>
+    <body>
+        <?php include("include/header.php"); ?> 
+        <div class="container">
+            <div class="row">
+                <h1 class="Section">
+                    <?php echo TXT_SECTION_ACTUALITES; ?>
+                </h1>
+            </div>
+            
+        <?php   $g = file_get_contents('./actualite/actualite-'.$lang.'-g.txt');
+                $d = file_get_contents('./actualite/actualite-'.$lang.'-d.txt');
+            if (empty($g) && empty($d)) {echo TXT_ACTUALITES_VIDE ;}
+            elseif (empty($g) || empty($d)) {
+                if(!empty($g)) {echo '<div class="col-sm-12 col-md-12">'.'<PRE>' .$g. '</PRE>'.'</div>';}
+                if(!empty($d)) {echo '<div class="col-sm-12 col-md-12">'.'<PRE>' .$d. '</PRE>'.'</div>';}
+                                            }
+            else {echo '<div class="col-sm-6 col-md-6">'.
+                '<PRE>' .$g. '</PRE>'.'</div>'.
+                
+                '<div class="col-sm-6 col-md-6">'.
+                '<PRE>' .$d. '</PRE>'.'</div>'
+                
+                ;}
+            ?>             
+             
+        </div>
+        <?php include("include/footer.php"); ?> 
+    </body>
+</html>
