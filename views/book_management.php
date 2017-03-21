f71c219b81fa27b788a80cf5de43a9dd2894737b
Doramadaire <guilhem-claverie@orange.fr>
2017-03-21 12:44:05 +0100

    on rassemble les fonctionnalités admin sur la même page

--- views/book_management.php
+++ views/book_management.php
@@ -36,28 +36,6 @@
 						</h1>					
 					</div>
 					<div class="row thumbnail">
-						<p><?php echo TXT_AJOUT_BOOK; ?></p>
-						<p><?php if (isset($dl_fail_error)) echo TXT_ERR_UPLOAD_FAIL; ?></p>
-						<p><?php if (isset($incorrect_file_extension_error)) echo TXT_ERR_INCORRECT_FILE_EXTENSION; ?></p>
-						<form action="book_management.php" method="post" enctype="multipart/form-data">
-							<label for="new_book_form"><?php echo TXT_AJOUT_BOOK2; ?></label><br>
-							<?php echo TXT_FICHIER_COMPLET; ?><input type="file" name="full_book_file" required/>
-							<?php echo TXT_FICHIER_EXTRAIT; ?><input type="file" name="extract_book_file" required/>
-							<!-- Autre façon de faire
-							<div class="fileUpload btn btn-primary">
-								<span>Upload</span>		
-								<input type="file" class="book_file" />						
-							</div				-->							
-							<input type="text" name="title" placeholder=<?php echo '"'.TXT_PLACEHOLDER_TITRE.'"'; ?> required><br>
-							<?php echo TXT_COLLECTION; ?> <select name="collection" required>
-								<option value="opening book"><?php echo TXT_COLLECTION_OPENINGBOOK; ?></option>
-								<option value="opening book photo"><?php echo TXT_COLLECTION_OPENINGBOOK_PHOTO; ?></option>
-							</select><br>
-							<?php echo TXT_ANNEE; ?><input type="number" name="year" value="<?php echo date("Y"); ?>" min="2015" required><br>
-							<input type="submit" class="btn btn-primary" name="new_book_form" value=<?php echo '"'.TXT_BOUTON_CREER_BOOK.'"'; ?>>	
-						</form>	 
-					</div>
-					<div class="row thumbnail">
 						<h3><?php echo TXT_ARTIST_BOOK_MANAGEMENT; ?></h3>
 						<a href="mailto:?subject=Le sujet&body=var_artiste a partagé avec vous son book paru dans la collection opening book. %0D%0A
 						Vous pouvez le consulter pendant un mois grâce au lien ci-dessous. %0D%0A var_lien  %0D%0A
--- views/user_management.php
+++ views/user_management.php
@@ -89,6 +89,29 @@
 							</form>	 
 						</div>
 
+						<div class="row thumbnail">
+							<p><?php echo TXT_AJOUT_BOOK; ?></p>
+							<p><?php if (isset($dl_fail_error)) echo TXT_ERR_UPLOAD_FAIL; ?></p>
+							<p><?php if (isset($incorrect_file_extension_error)) echo TXT_ERR_INCORRECT_FILE_EXTENSION; ?></p>
+							<form action="book_management.php" method="post" enctype="multipart/form-data">
+								<label for="new_book_form"><?php echo TXT_AJOUT_BOOK2; ?></label><br>
+								<?php echo TXT_FICHIER_COMPLET; ?><input type="file" name="full_book_file" required/>
+								<?php echo TXT_FICHIER_EXTRAIT; ?><input type="file" name="extract_book_file" required/>
+								<!-- Autre façon de faire
+								<div class="fileUpload btn btn-primary">
+									<span>Upload</span>		
+									<input type="file" class="book_file" />						
+								</div				-->							
+								<input type="text" name="title" placeholder=<?php echo '"'.TXT_PLACEHOLDER_TITRE.'"'; ?> required><br>
+								<?php echo TXT_COLLECTION; ?> <select name="collection" required>
+									<option value="opening book"><?php echo TXT_COLLECTION_OPENINGBOOK; ?></option>
+									<option value="opening book photo"><?php echo TXT_COLLECTION_OPENINGBOOK_PHOTO; ?></option>
+								</select><br>
+								<?php echo TXT_ANNEE; ?><input type="number" name="year" value="<?php echo date("Y"); ?>" min="2015" required><br>
+								<input type="submit" class="btn btn-primary" name="new_book_form" value=<?php echo '"'.TXT_BOUTON_CREER_BOOK.'"'; ?>>	
+							</form>	 
+						</div>
+
 						<div class="row">
 						    <h1 class="Section">Mettre à jour les actualités</h1>
 						</div>
