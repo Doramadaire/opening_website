aab1c2cfe6b14fab1a8984a4ca55d21279cbf335
Doramadaire <guilhem-claverie@orange.fr>
2017-03-28 18:12:15 +0200

    marge page about

--- views/about.php
+++ views/about.php
@@ -3,13 +3,13 @@
     <head>
         <?php include("include/html_header.php"); ?>
         <title><?php echo TXT_ONGLET; ?></title>
-        <link href="css/about.css" rel="stylesheet">
+        <!-- <link href="css/about.css" rel="stylesheet"> -->
     </head>
     <body>
         <?php include("include/header.php"); ?> 
         <div class="container-fluid row">
-            <div class="col-md-1"></div>
-            <div class="col-md-10">
+            <div class="col-xs-1"></div>
+            <div class="col-xs-10">
                 <div class="row">
                     <h1 class="Section">
                         <?php echo TXT_SECTION_EDITO; ?>
@@ -19,7 +19,7 @@
                     <?php echo TXT_EDITO_TEXTE; ?>
                 </div>
             </div>
-            <div class="col-md-1"></div>
+            <div class="col-xs-1"></div>
         </div>
         <?php include("include/footer.php"); ?> 
     </body>
